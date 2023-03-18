492bd314f9f78a372ef82f5ba51401f8957356e0
nick <mwauranicholas10@gmail.com>
2023-03-18 12:48:26 +0300

    completed assignment

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,13 @@
-class Cheese < ApplicationRecord
-  
-  def summary
-    "#{self.name}: $#{self.price}"
+class CheesesController < ApplicationController
+
+  def index
+    cheeses = Cheese.all
+    render json: cheeses
+  end
+
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
   end
 
 end
